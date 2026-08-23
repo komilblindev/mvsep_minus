358c29744b0630bb8d531959a5f2472bf89de7d5
Komil Hamzayev <hamzayevkomil52@gmail.com>
2026-08-23 23:48:04 +0500

    fix: remove suicidal installTasks.py logic

--- addon/installTasks.py
+++ addon/installTasks.py
@@ -7,7 +7,4 @@ import os
 addonHandler.initTranslation()
 
 def onInstall():
-	for addon in addonHandler.getAvailableAddons():
-		if addon.manifest['name'] == "mvsep_minus" and addon.isPendingRemove == False:
-			if addon.manifest['version'] != "1.0.0":
-				addon.requestRemove()
+	pass
